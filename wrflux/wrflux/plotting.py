8601c7f9a178b42211638a284588658c4c4b4d89
Matthias Göbel <matthias.goebel@uibk.ac.at>
2021-01-20 15:49:41 +0100

    deleted unused profile plots and improved savefig

--- wrflux/wrflux/plotting.py
+++ wrflux/wrflux/plotting.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Functions for plotting WRFlux output data:
-tendency profiles and scatter plots
+scatter plots
 
 @author: Matthias Göbel
 """
@@ -10,7 +10,6 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from wrflux import tools
-import seaborn as sns
 import xarray as xr
 xr.set_options(arithmetic_join="exact")
 xr.set_options(keep_attrs=True)
@@ -19,141 +18,6 @@ tex_names = {"t": "\\theta", "q": "q_\\mathrm{v}",
              "u": "u", "v": "v", "v": "v"}
 
 # %%
-
-# TODOm possible with standard seaborn?
-
-
-def tend_prof(dat, var, loc=None, iloc=None, rename=None, extra_xaxis=None,
-              rolling_xwindow=None, rolling_dim=None, zmax=2000, multiplier=1,
-              sharex=True, xlim=None, **kwargs):
-    """Plot tendency profiles for variable var using seaborn's relplot with all its
-    options for  grouping by style, hue, and size and faceting.
-
-
-    Parameters
-    ----------
-    dat : xarray DataArray
-        Tendency components for variable var.
-    var : str
-        Variable to plot.
-    loc : dict, optional
-        Mapping for label based indexing before plotting. The default is None.
-    iloc : dict, optional
-        Mapping for integer-location based indexing before plotting. The default is None.
-    rename : dict, optional
-        Rename coordinates. The default is None.
-    extra_xaxis : dict, optional
-        Mapping from coordinates to keys for which to draw separate x-axis in each subplot.
-        The default is None.
-    rolling_xwindow : float, optional
-        If given, do rolling average in dimension "rolling_dim" with the given window size.
-        The default is None.
-    rolling_dim : str, optional
-        Dimension for rolling mean. The default is None.
-    zmax : float, optional
-        Upper limit of y-axis: height (m). The default is 2000.
-    multiplier : float, optional
-        Multiply data with this factor. The default is 1.
-    sharex : bool, optional
-        Share x-axis among subplots. The default is True.
-    xlim : tuple of float (xmin, xmax), optional
-        Set lower and upper limit of x-axis . The default is None.
-    **kwargs :
-        Keyword arguments passed to seaborn's relplot.
-
-    Returns
-    -------
-    pgrid : FacetGrid
-
-    """
-    attrs_v = dat.attrs.copy()
-    if rolling_xwindow is not None:
-        dx = dat[rolling_dim][-1] - dat[rolling_dim][-2]
-        rolling_xwindow = int(rolling_xwindow / dx) + 1
-        dat = tools.rolling_mean(dat, rolling_dim, rolling_xwindow, periodic=True, center=True)
-
-    dat = dat.where(dat.hgt < zmax)
-    if "bottom_top" in dat.dims:
-        dat = dat.dropna("bottom_top", "all")
-    else:
-        dat = dat.dropna("bottom_top_stag", "all")
-    dat = multiplier * dat
-
-    dat = tools.loc_data(dat, loc=loc, iloc=iloc)
-
-    for d in tools.xy:
-        if d in dat.coords:
-            dat = dat.rename({d: d.upper()})
-
-    if rename is not None:
-        dat = dat.rename(rename)
-
-    if kwargs["hue"] == "comp":
-        kwargs["palette"] = {"adv_r": "gray", "mean": "tab:blue", "trb_r": "tab:olive",
-                             "trb_s": "tab:orange", "trb": "tab:red", "net": "black",
-                             "forcing": "violet", "rad": "tab:green", "rad+trb": "tab:brown",
-                             "rad_sw": "green", "rad_lw": "lime",
-                             "pg": "tab:green", "cor_curv": "tab:brown", "mp": "cyan"}
-
-    datp = dat.copy()
-    df = datp.to_dataframe(name="tend").reset_index()
-
-    sns.set_style("whitegrid")
-    if extra_xaxis is not None:
-        # exclude locations in extra_xaxis from plot but not from legend
-        df = df.copy()
-        for key, val in extra_xaxis.items():
-            df["tend"] = df["tend"].where(df[key] != val, np.inf)
-    pgrid = sns.relplot(data=df, kind="line", x="tend", y="hgt", sort_dim="y",
-                        facet_kws={"sharex": sharex, "margin_titles": True, "legend_out": True},
-                        **kwargs)
-
-    pgrid.set_xlabels("")
-    pgrid.set_ylabels("height above ground (m)")
-    pax = pgrid.axes
-
-    if sharex == "row":
-        display_ticks(pax)
-    middle_column = int((len(pax[0]) - 1) / 2)
-    label = var
-    if var in tex_names:
-        label = tex_names[var]
-
-    t = "tendency"
-    if t not in attrs_v["description"]:
-        t = "flux"
-
-    mult = ""
-    if multiplier != 1:
-        power = np.log10(1 / multiplier)
-        if int(power) == power:
-            power = int(power)
-        mult = "10$^{%s}$ " % power
-    pax[-1, middle_column].set_xlabel("$%s$ %s components (%s%s)" % (label, t, mult,
-                                                                     attrs_v["units"]))
-    pax_flat = pax.flatten()
-    if extra_xaxis is not None:
-        # plot locations in extra_xaxis with separate x-axis for each subplot
-        df = datp.loc[extra_xaxis].to_dataframe(name="tend").reset_index()
-        kwargs_sub = {}
-        for k in kwargs:
-            if k not in ["aspect", "height", "col", "row"]:
-                kwargs_sub[k] = kwargs[k]
-        sns.set_style("ticks")
-        pax2 = []
-        for ax in pax_flat:
-            ax2 = ax.twiny()
-            pax2.append(ax2)
-            sns.lineplot(data=df, legend=False, ax=ax2, x="tend", y="hgt", sort_dim="y", **kwargs_sub)
-        pax_flat = [*pax_flat, *pax2]
-        pax2 = np.array(pax2).reshape(pax.shape)
-        pax2[-1, middle_column].set_xlabel("$%s$ %s components sum (%s%s)" % (label, t, mult,
-                                                                              attrs_v["description"]))
-
-    if xlim is not None:
-        pgrid.set(xlim=xlim)
-
-    return pgrid
 
 
 def scatter_hue(dat, ref, plot_diff=False, hue="bottom_top", ignore_missing_hue=False,
@@ -313,17 +177,13 @@ def scatter_hue(dat, ref, plot_diff=False, hue="bottom_top", ignore_missing_hue=
             os.makedirs(figloc, exist_ok=True)
         if fname is None:
             fname = "scatter"
-        if "." not in fname:
-            fname += ".png"
-        fig.savefig(figloc + "/" + fname, dpi=300, bbox_inches="tight")
-    plt.show()
+        fpath = figloc + "/" + fname
+        try:
+            fig.savefig(fpath, dpi=300, bbox_inches="tight")
+        except ValueError:
+            fig.savefig(fpath + ".png", dpi=300, bbox_inches="tight")
+
     if close:
         plt.close()
 
     return fig, ax, cax
-
-
-def display_ticks(axes):
-    plt.subplots_adjust(hspace=0.2)
-    for ax in axes.flatten():
-        ax.xaxis.set_tick_params(labelbottom=True)
